789db79fbb006ddec879ee5365348b7fb767fd34
sakshi kashte <Sakshikashte67@gmail.com>
2025-04-12 22:02:21 +0530

    new changes

--- task/src/Components/UserCard.js
+++ task/src/Components/UserCard.js
@@ -1,29 +1,31 @@
 import React from "react";
 
-const UserCard=({user})=>{
-    return(
-        <>
-        <div className="user-card">
-            <div className="flex items-center space-x-4">
-            {/* <img
+const UserCard = ({ user }) => {
+  return (
+    <>
+      <div className="user-card">
+        <div className="flex items-center space-x-4">
+          {/* <img
             src={`https://ui-avatars.com/api/?name=${encodeURIComponent(user.name)}&background=random`}
           alt="Profile"
           className="profile-img"
         /> */}
-        <div className="rounded-full w-16 h-16 bg-gray-200 flex items-center justify-center text-4xl">
-  👤
-</div>
+          <img src="https://www.gravatar.com/avatar/?d=mp" alt="Profile" />
 
-        <div>
+          {/* <div className="rounded-full w-16 h-16 bg-gray-200 flex items-center justify-center text-4xl">
+            👤
+          </div> */}
+
+          <div>
             <h2>{user.name}</h2>
             <p>{user.role}</p>
             <p>{user.location}</p>
+          </div>
         </div>
-            </div>
-            <button>Contact</button>
-        </div>
-        </>
-    )
-}
+        <button>Contact</button>
+      </div>
+    </>
+  );
+};
 
 export default UserCard;
